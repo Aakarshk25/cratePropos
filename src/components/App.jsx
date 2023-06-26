1d3fda354ae789358f08e9157fc1f06e36d06dd9
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2023-06-25 23:24:55 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Image from "./Image";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Image img="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
+      {/* // there will be no ; this sign in the ind  */}
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
